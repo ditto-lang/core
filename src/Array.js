0165be1a87f7acc4eb7cdb1302d49375e6e904be
Jordan Mackie <jmackie@protonmail.com>
2020-06-16 12:18:06 +0100

    Fixup Array.js

--- src/Array.js
+++ src/Array.js
@@ -1,8 +1,11 @@
-const index_impl = nope => yep => i => arr => i < arr.length ? yep(arr[i]) : nope;
+exports.index_impl = (nope) => (yep) => (i) => (arr) =>
+  i < arr.length ? yep(arr[i]) : nope;
 
-const zip_with_impl = f => xs => ys => 
-  Array.from({ length: Math.min(xs.length, ys.length) }, (_, i) => f(xs[i])(ys[i]);
-
-// NOTE: better to start with newer, more lightweight syntax as it can then be
-// passed through babel/polyfilled/whatever for the _browsers that need it_. 
-// The _reverse_ isn't possible (i.e. making syntax slimmer).
+exports.zip_with_impl = (f) => (xs) => (ys) => {
+  const len = Math.max(xs.length, ys.length);
+  const zs = new Array(len);
+  for (let i = 0; i < len; i++) {
+    zs[i] = f(xs[i])(ys[i]);
+  }
+  return zs;
+};
