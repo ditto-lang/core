00a63c3ad82809f8c0fcffff3a66ae3a47c7023d
Jordan Mackie <jmackie@protonmail.com>
2020-07-08 13:00:56 +0100

    Use js modules

--- src/Array.mjs
+++ src/Array.mjs
@@ -1,7 +1,7 @@
-exports.index_impl = (nope) => (yep) => (i) => (arr) =>
+export const index_impl = (nope) => (yep) => (i) => (arr) =>
   i < arr.length ? yep(arr[i]) : nope;
 
-exports.zip_with_impl = (f) => (xs) => (ys) => {
+export const zip_with_impl = (f) => (xs) => (ys) => {
   const len = Math.max(xs.length, ys.length);
   const zs = new Array(len);
   for (let i = 0; i < len; i++) {
